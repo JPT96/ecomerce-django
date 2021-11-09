cdd16f933f7c74313ea5d4dc74d7282f023920a1
JPT96 <joshuathomas96@msn.com>
2021-11-09 18:57:23 +0000

    created super user

--- static/views.py
+++ static/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, httpResponse
+from django.shortcuts import render 
 
 # Create your views here.
 def say_hello(request):
-    return httpResponse("hello")
+    return render(request, "static/static.html")
